182cfefafa2d0faeb857281a7c981a7fdd903a8f
ziogianni <ziogianni@gmail.com>
2017-08-16 20:22:02 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -73,6 +73,7 @@ function puckInteract(todo){
       todo.qty = trimmedQty.toString();
       db.get(todo._id).then(function(doc) {
        return db.put({
+       _rev: doc._rev  
        qty: todo.qty
       });
       //db.put(todo);
